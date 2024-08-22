4531b7f4ef1a769f45fc027a659ad8993cb8964c
ogi1118 <aratanew2525@gmail.com>
2024-08-23 07:01:17 +0900

    サイドバーコンポーネントの追加

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <AppSidebar />
+    <div class="main-content">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import AppSidebar from './components/AppSidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppSidebar,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  display: flex;
+  height: 100vh; /* ビューポートの高さを100%に設定 */
+  width: 100%; /* ビューポートの幅を100%に設定 */
+}
+
+
+.main-content {
+  display: flex;
+  flex-direction: column; /* 縦方向に配置 */
+  align-items: center; /* 横方向に中央配置 */
+  justify-content: flex-start; /* 上方向に詰めて配置 */
+  padding: 20px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  flex: 1;
+}
+</style>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,112 @@
+<!-- components/AppSidebar.vue -->
+<template>
+  <div class="appsidebar">
+    <button class="mypage-button">マイページ</button>
+    <ul>
+      <li><a href="#">ホーム</a></li>
+      <li><a href="#">アバウト</a></li>
+      <li><a href="#">サービス</a></li>
+      <li><a href="#">お問い合わせ</a></li>
+      <li v-for="(item, index) in userContent" :key="index">
+        <a href="#">{{ item }}</a>
+      </li>
+    </ul>
+    <div class="add-content">
+      <input type="text" v-model="newContent" placeholder="新しい項目を追加" @keyup.enter="addContent" />
+      <button @click="addContent">追加</button>
+    </div>
+    <button class="setting-button">設定</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSidebar",
+  data() {
+    return {
+      newContent: "",
+      userContent: []
+    };
+  },
+  methods: {
+    addContent() {
+      if (this.newContent.trim() !== "") {
+        this.userContent.push(this.newContent.trim());
+        this.newContent = ""; // 入力フィールドをクリア
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.appsidebar {
+  height: 100%;
+  background-color: #f4f4f4;
+  padding: 0;
+  box-shadow: 2px 0 5px rgba(0,0,0,0.1);
+  width: 200px;
+  position: relative;
+}
+
+.setting-button {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 10px;
+  background-color: #9f9c9c;
+  color: #fff;
+  text-align: center;
+}
+
+.mypage-button{
+  width: 100%;
+  background-color: #9f9c9c;
+  color: #fff;
+  height: 60px;
+  border: none;
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 20px 0;
+}
+
+li {
+  margin: 10px 0;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.add-content {
+  padding: 10px;
+}
+
+.add-content input {
+  width: calc(100% - 80px);
+  padding: 5px;
+  margin-right: 5px;
+}
+
+.add-content button {
+  padding: 5px 10px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.add-content button:hover {
+  background-color: #555;
+}
+</style>
